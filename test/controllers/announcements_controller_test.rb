d057bceab9a1f30dc0aa972f9a66b3e4f825a31c
cfaloon <cfaloon@gmail.com>
2018-04-27 14:05:13 -0400

    Another controller test

--- test/controllers/announcements_controller_test.rb
+++ test/controllers/announcements_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class AnnouncementsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'redirected if not authenticated' do
     get announcements_path
     assert_redirected_to new_user_session_path
+  end
+
+  test 'logged in beginner views index' do
+    sign_in create(:beginner)
+    get announcements_path
+    assert_response :success
+    assert_match '<h2>Announcements</h2>', @response.body
   end
 end
