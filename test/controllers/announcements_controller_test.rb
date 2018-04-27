d3e42ef7bb88272f98d4b2f033b26917bbb01b69
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-04-27 13:57:29 -0400

    Pass another test

--- test/controllers/announcements_controller_test.rb
+++ test/controllers/announcements_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AnnouncementsControllerTest < ActionDispatch::IntegrationTest
   test 'redirected if not authenticated' do
-    get anouncements_path
+    get announcements_path
     assert_redirected_to new_user_session_path
   end
 end
