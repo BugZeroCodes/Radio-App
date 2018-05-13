55e969f113009b128e385f4bfd4cc5da2659e6b0
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-05-13 16:02:56 -0400

    Make new announcement pages

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,4 +4,27 @@ class AnnouncementsController < ApplicationController
   def index
     @announcements = Announcement.all
   end
+
+  def new
+    @announcement = Announcement.new
+  end
+
+  def show
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def create
+    @announcement = Announcement.new(announcement_params)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_announcement
+      @announcement = announcement.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def announcement_params
+      params.require(:announcement).permit(:text, :expires_at)
+    end
 end
