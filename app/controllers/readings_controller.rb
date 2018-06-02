2df84be7efa91c4723edba88851a0a368ce28f9f
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-06-02 12:39:21 -0400

    Make pages for readings

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,9 +1,11 @@
 class ReadingsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @readings = Reading.all
   end
 
   def show
+    @reading = Reading.find(params[:id])
   end
 
   def new
